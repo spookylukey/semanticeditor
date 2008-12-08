9c80a2195137fbbbe7dc2fca2b4285d3dd54bb3c
Luke Plant <L.Plant.98@cantab.net>
2008-12-08 11:36:47 +0000

    Updated CSS class model to have both a short name and long description

--- semanticeditor/models.py
+++ semanticeditor/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 class CssClass(models.Model):
-    class_name = models.CharField("CSS class name", max_length=255, unique=True)
-    description = models.CharField("Description", max_length=255, blank=True)
+    class_name = models.CharField("CSS class name", max_length=255, unique=True,
+                                  help_text="The name as it appears in the CSS file")
+    name = models.CharField("Name", max_length=255, blank=False, unique=True,
+                            help_text="User-visible name of the style.")
+    description = models.TextField("Description", max_length=255, blank=True,
+                                   help_text="This is displayed as a 'tooltip' "
+                                   "when the user hovers over the name in the "
+                                   "list of styles.  The data will be interpreted "
+                                   "as raw HTML, so it can include an example.")
 
     def __unicode__(self):
-        return self.class_name
+        return self.name
 
     class Meta:
         verbose_name = "CSS class"
