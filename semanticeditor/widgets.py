6cd7bc75a3d3088332e376ef8b9029adca2b0977
Luke Plant <L.Plant.98@cantab.net>
2009-03-10 18:52:21 +0000

    Updated editor to match django-cms2 editor.

--- semanticeditor/widgets.py
+++ semanticeditor/widgets.py
@@ -1,6 +1,8 @@
-from cms.plugins.text.widgets import WYMEditor
+from cms.admin.editors.wymeditor.widgets import WYMEditor
+from cms.admin.editors.wymeditor import settings
+from cms.settings import CMS_MEDIA_URL
 from django import forms
-from django.conf import settings
+from django.conf import settings as global_settings
 from django.utils.safestring import mark_safe
 from django.utils.translation.trans_real import get_language
 from django.template.loader import render_to_string
@@ -10,7 +12,7 @@ join = os.path.join
 
 class SemanticEditor(WYMEditor):
     class Media:
-        js = [join(settings.SE_MEDIA_URL, path) for path in
+        js = [join(global_settings.SE_MEDIA_URL, path) for path in
               ('javascript/wymeditor/plugins/semantic/wymeditor.semantic.js',
                'javascript/json2.js',
                'javascript/orbitaltooltip.js',
@@ -21,7 +23,11 @@ class SemanticEditor(WYMEditor):
         context = {
             'name': name,
             'language': language,
-            'SE_MEDIA_URL': settings.SE_MEDIA_URL,
+            'SE_MEDIA_URL': global_settings.SE_MEDIA_URL,
+            'CMS_MEDIA_URL': CMS_MEDIA_URL,
+            'WYM_TOOLS': mark_safe(settings.WYM_TOOLS),
+            'WYM_CONTAINERS': mark_safe(settings.WYM_CONTAINERS),
+            'WYM_CLASSES': mark_safe(settings.WYM_CLASSES),
         }
 
         return mark_safe(render_to_string(
