c8eac29707913e2b5f486bd132aed6a16fe7165b
Luke Plant <L.Plant.98@cantab.net>
2011-04-18 15:55:12 +0100

    Added inline editing of CssClass in CssClassCategory
    Change from Daniele

--- semanticeditor/admin.py
+++ semanticeditor/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
 from semanticeditor.models import CssClass, CssClassCategory
 
+class CssClassAdminInline(admin.StackedInline):
+    model = CssClass
+    extra = 0
+    fieldsets = (
+        (None, {
+            'fields': (
+                ('name', 'verbose_name'),
+                ('description', 'templates',),
+                ('allowed_elements', 'column_equiv'),
+            ),
+        }),
+    )
 
 class CssClassAdmin(admin.ModelAdmin):
     list_display = ('verbose_name', 'name', 'category')
 
-
+class CssClassCategoryAdmin(admin.ModelAdmin):
+    inlines = [CssClassAdminInline,]
+    
 admin.site.register(CssClass, CssClassAdmin)
-admin.site.register(CssClassCategory)
+admin.site.register(CssClassCategory, CssClassCategoryAdmin)
